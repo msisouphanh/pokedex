ab36bbb2ba154a7174a3060d7a0128b192e48f0f
Marcus Sisouphanh <107326899+msisouphanh@users.noreply.github.com>
2025-12-18 17:46:36 -0500

    Displaying pokemon names

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,22 +1,36 @@
 import { useEffect, useState } from "react";
 
+export interface Pokemon {
+  id: number;
+  name: string;
+}
+
+export interface Page {
+  count: number;
+  next: string;
+  previous: string;
+  results: Pokemon[];
+}
+
 function PokemonList() {
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
   const url = "https://pokeapi.co/api/v2/pokemon/";
   const [currentUrl, setUrl] = useState(url);
 
   useEffect(() => {
     fetch(currentUrl)
       .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
+      .then((data: Page) => {
+        setPokemon((prev) => [...prev, ...data.results]);
       });
   }, [currentUrl]);
 
+  console.log(pokemon);
   return (
     <>
-      {/* {currentData.map((item) => (
-        <p>{item.results.name}</p>
-      ))} */}
+      {pokemon.map((item) => (
+        <p>{item.name}</p>
+      ))}
     </>
   );
 }
