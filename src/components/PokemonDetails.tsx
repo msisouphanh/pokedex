3ffcafb56af1d180553dd01076996e4cfa0587ba
Marcus Sisouphanh <107326899+msisouphanh@users.noreply.github.com>
2026-01-05 19:16:52 -0500

    Added pokemon types

--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -1,11 +1,16 @@
 import { useLocation } from "react-router-dom";
+import { type Pokemon } from "../App.tsx"; // or wherever your types live
 
 function PokemonDetails() {
   const location = useLocation();
-  const pkm = location.state;
+  const pkm = location.state as Pokemon;
+  const multiplier = 0.220462;
+
   return (
     <>
       <h1>{pkm.name}</h1>
+      <p>Weight: {parseFloat((pkm.weight * multiplier).toFixed(1))} lbs</p>
+      <p>Types: {pkm.types.map((t) => t.type.name)} </p>
     </>
   );
 }
