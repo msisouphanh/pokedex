c08287c15001454f4cd3a1d997f55b7c2e41fe07
Marcus Sisouphanh <107326899+msisouphanh@users.noreply.github.com>
2026-01-11 17:23:07 -0500

    Update PokemonDetails.tsx

--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -1,5 +1,4 @@
 import { useParams, Link, useLocation } from "react-router-dom";
-import { type Pokemon, type PokemonType } from "../App.tsx";
 import { useState, useEffect } from "react";
 import { typeColors } from "../TypeColors";
 import type { PkmType } from "../TypeColors";
