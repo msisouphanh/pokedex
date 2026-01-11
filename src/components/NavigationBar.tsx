d0c94fffeb0834d28670e9e7c656d26b579329f5
Marcus Sisouphanh <107326899+msisouphanh@users.noreply.github.com>
2026-01-11 17:25:35 -0500

    deploy

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import pokemonLogo from "../assets/images/pokemonlogo.png";
 
 function NavigationBar() {
   return (
@@ -6,10 +7,7 @@ function NavigationBar() {
       <div className="w-full mb-8">
         <div className="flex flex-row items-center justify-center max-w-6xl mx-auto">
           <Link to="/">
-            <img
-              className="h-[150px]"
-              src="../../public/images/pokemonlogo.png"
-            />
+            <img className="h-[150px]" src={pokemonLogo} />
           </Link>
         </div>
       </div>
--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -1,4 +1,5 @@
 import { useParams, Link, useLocation } from "react-router-dom";
+import { type Pokemon } from "../App.tsx";
 import { useState, useEffect } from "react";
 import { typeColors } from "../TypeColors";
 import type { PkmType } from "../TypeColors";
